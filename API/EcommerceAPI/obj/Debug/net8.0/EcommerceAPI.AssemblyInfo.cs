b1c85330e9aae9ac4666fee2ecfacf513fd6d8b0
Anh Nguyen Viet <anhnguyenv1@nashtechglobal.com>
2024-11-25 01:22:52 +0000

    minor update1

--- API/EcommerceAPI/obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
+++ API/EcommerceAPI/obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e087512aedeabbf56313c9feb3bba612a09ab9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def27ab4c837733bacb9dca0a3f26337f0ef9f2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
